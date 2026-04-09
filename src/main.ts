027cb526b71881f84cd56df6bdc8a62151d91dfc
jérôme <jerome@gmail.com>
2026-04-09 17:07:53 +0000

    feat:add Frontend production Vercel

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
       'http://127.0.0.1:3000',
       'http://localhost:5173',
       'http://127.0.0.1:5173',
+      'https://autodrive-five.vercel.app',
     ];
 
     if (frontendUrl) {
